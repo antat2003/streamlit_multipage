c70c12f15734a58d8320e8a4b7a110d664df85fe
Tat, An <antat2003@yahoo.com>
2021-09-12 22:25:41 -0500

    change to selectbox

--- multipage.py
+++ multipage.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 from pages import home
+import uuid
 
 class MultiPage:
 
@@ -17,27 +18,16 @@ class MultiPage:
 
         self.pages.append({
             "title": title,
-            "function": func,
-            "current": False
+            "function": func
         })
 
     def run(self):
-        default = True
-        st.sidebar.markdown("App Navigation")
-        
-        nav_button = st.markdown(""" <style> div.stButton > button:first-child { border:none; background:none; } </style>""", unsafe_allow_html=True) 
-        nav_button = st.markdown(""" <style> .css-1g71wml:focus:not(:active) { border-color: rgb(0, 0, 0); color: #602ff1; } </style>""", unsafe_allow_html=True) 
-        nav_button = st.markdown(""" <style> .css-1g71wml:focus { box-shadow: none; border-color: rgb(0, 0, 0); } </style>""", unsafe_allow_html=True) 
-        nav_button = st.markdown(""" <style> .css-1g71wml:hover { box-shadow: rgb(0, 0, 0); color: rgb(0, 0, 0); outline:none; } </style>""", unsafe_allow_html=True) 
-            
-        for page in self.pages:
-            
-            if st.sidebar.button(page['title']):
-                page['function']()
-                default = False
-        
-        if default:
-            home.app()
+        app = st.sidebar.selectbox(
+            'App Navigation',
+            self.pages,
+            format_func=lambda app: app['title'])
+
+        app['function']()
 
         
 
